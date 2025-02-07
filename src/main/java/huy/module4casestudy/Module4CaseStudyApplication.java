bb11ba06b6e557a94e525675d247569a90c5735c
AnHuy <92672519+danganhuy@users.noreply.github.com>
2025-02-07 09:21:43 +0700

    init

---
+++ src/main/java/huy/module4casestudy/Module4CaseStudyApplication.java
@@ -0,0 +1,11 @@
+package huy.module4casestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module4CaseStudyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Module4CaseStudyApplication.class, args);
+    }
+}
---
+++ src/test/java/huy/module4casestudy/Module4CaseStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package huy.module4casestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module4CaseStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
